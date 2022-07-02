f447e3c3d5b9155b46f265f103aa3fed9a1a329c
Will Myers <56778753+willmyethewebsiteguy@users.noreply.github.com>
2022-07-02 19:56:06 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@
 (function () {
   const ps = {
     cssId: 'wm-accordions',
-    cssFile: 'https://cdn.jsdelivr.net/gh/willmyethewebsiteguy/accordions@3.1.010/styles.min.css'
+    cssFile: 'https://cdn.jsdelivr.net/gh/willmyethewebsiteguy/accordions@3.1.011/styles.min.css'
   };
   const defaults = {
     icons: {
